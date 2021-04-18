ba0578654d0544f5218b411855c00ebf0af0ce23
LuLaValva <lulavalva@gmail.com>
2021-04-18 07:33:17 -0400

    Updated icon

--- pages/keyboard.js
+++ pages/keyboard.js
@@ -4,14 +4,24 @@ import pageStyles from "../styles/page.module.css";
 import StandardFooter from "./Components/standardFooter";
 
 export default class Keyboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      recording: false,
+      recordedNotes: [],
+      audioContext: new AudioContext(),
+      audioBuffer: null,
+    };
+  }
+
   render() {
     return (
       <div className={pageStyles.container}>
         <Head>
           <title>Keyboard - TensorBeat</title>
-          <link rel="icon" href="favicon.io" />
+          <link rel="icon" href="/tensorbeat.svg" />
         </Head>
-
+        <button onClick={this.playSound(800)}></button>
         <StandardFooter />
       </div>
     );
