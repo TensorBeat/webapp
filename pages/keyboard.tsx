54c90b588890d09a708d52997fac74a0664c2664
Mark Vadeika <markvadeika@gmail.com>
2021-04-23 01:13:35 -0400

    indented with four spaces

--- pages/keyboard.tsx
+++ pages/keyboard.tsx
@@ -1,237 +1,239 @@
 import Head from "next/head";
-import React, { Component } from "react";
+import React, {Component} from "react";
 import pageStyles from "../styles/page.module.css";
 import boxStyles from "../styles/box.module.css";
 import StandardFooter from "../components/standardFooter";
 // @ts-ignore
-import { KeyboardShortcuts, MidiNumbers } from "react-piano";
+import {KeyboardShortcuts, MidiNumbers} from "react-piano";
 import "react-piano/dist/styles.css";
 import SoundFontProvider from "../components/soundFontProvider";
 import PianoWithRecording from "../components/pianoWithRecording";
 
-import { v4 as uuidv4 } from "uuid";
-import { GenerateMusicRequest } from "../grpc-web/tensorbeat/sarosh_gen_pb";
-import { SaroshGeneratorClient } from "../grpc-web/tensorbeat/Sarosh_genServiceClientPb";
+import {v4 as uuidv4} from "uuid";
+import {GenerateMusicRequest} from "../grpc-web/tensorbeat/sarosh_gen_pb";
+import {SaroshGeneratorClient} from "../grpc-web/tensorbeat/Sarosh_genServiceClientPb";
 
 const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net";
 
 const noteRange = {
-  first: MidiNumbers.fromNote("c3"),
-  last: MidiNumbers.fromNote("f4"),
+    first: MidiNumbers.fromNote("c3"),
+    last: MidiNumbers.fromNote("f4"),
 };
 const keyboardShortcuts = KeyboardShortcuts.create({
-  firstNote: noteRange.first,
-  lastNote: noteRange.last,
-  keyboardConfig: KeyboardShortcuts.HOME_ROW,
+    firstNote: noteRange.first,
+    lastNote: noteRange.last,
+    keyboardConfig: KeyboardShortcuts.HOME_ROW,
 });
 
 interface Frame {
-  id: string;
-  notes: string;
-  start: number;
-  duration: number;
+    id: string;
+    notes: string;
+    start: number;
+    duration: number;
 }
 
 interface KeyboardState {
-  recording: Frame[];
-  activeNotes: string[];
-  audioContext: any;
-  generatedMusic: string[] | undefined;
-  musicIsGenerating: boolean;
+    recording: Frame[];
+    activeNotes: string[];
+    audioContext: any;
+    generatedMusic: string[] | undefined;
+    musicIsGenerating: boolean;
 }
-interface KeyboardProps {}
+
+interface KeyboardProps {
+}
 
 const PIANO_WIDTH = 500;
 const NOTE_DURATION = 200; // milliseconds
 const SORTED_PITCHES = [
-  "C",
-  "C#",
-  "D",
-  "D#",
-  "E",
-  "F",
-  "F#",
-  "G",
-  "G#",
-  "A",
-  "A#",
-  "B",
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
 ];
 
 export default class Keyboard extends Component {
-  state: KeyboardState = {
-    audioContext: null,
-    recording: [],
-    activeNotes: [],
-    generatedMusic: undefined,
-    musicIsGenerating: false,
-  };
-
-  scheduledEvents: [];
-
-  componentDidMount = () => {
-    this.setState({
-      // audioContext: new (window.AudioContext || window.webkitAudioContext)(),
-      audioContext: new window.AudioContext(),
-    });
-  };
-
-  constructor(props: KeyboardProps) {
-    super(props);
-    this.scheduledEvents = [];
-  }
-
-  appendToRecording = (item: {
-    notes: string;
-    start: number;
-    duration: number;
-  }) => {
-    const { notes, start, duration } = item;
-
-    const frame: Frame = {
-      id: uuidv4(),
-      notes: notes,
-      start: start,
-      duration: duration,
+    state: KeyboardState = {
+        audioContext: null,
+        recording: [],
+        activeNotes: [],
+        generatedMusic: undefined,
+        musicIsGenerating: false,
     };
 
-    this.setState({
-      recording: [...this.state.recording, frame],
-    });
-  };
+    scheduledEvents: [];
 
-  clearRecording = () => {
-    this.setState({ recording: [] });
-  };
-
-  playRecording = () => {
-    const initialTime = this.state.recording[0].start;
-    this.state.recording.forEach(({ id, notes, start, duration }) => {
-      let startDelay = start - initialTime;
-      setTimeout(() => {
-        this.setState({ activeNotes: [] }); // Ensure that repeated notes are played again
-        this.setState({ activeNotes: notes.split(".") });
-      }, startDelay);
-      setTimeout(() => {
-        // TODO: make this work with id's. Might not be needed though
+    componentDidMount = () => {
         this.setState({
-          activeNotes: this.state.activeNotes.filter(
-            (an) => !notes.split(".").includes(an)
-          ),
+            // audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+            audioContext: new window.AudioContext(),
         });
-      }, startDelay + duration);
-    });
-  };
+    };
 
-  playGeneratedMusic = () => {
-    this.state.generatedMusic?.forEach((note, index) => {
-      if (note.includes(".")) {
-        // chords start at 0, so transpose them up to c4
-        note = note.split(".").map(n => n + MidiNumbers.fromNote("c4")).join(".")
-      }
-      // generator outputs flats as "-"
-      note.replace("-", "b");
-      setTimeout(() => {
-        this.setState({ activeNotes: [] });
-        this.setState({ activeNotes: note.split(".") });
-      }, index * NOTE_DURATION);
-    });
-  };
+    constructor(props: KeyboardProps) {
+        super(props);
+        this.scheduledEvents = [];
+    }
 
-  sendRecordingToGenerator = () => {
-    const client = new SaroshGeneratorClient("http://grpc-web.tensorbeat.com");
-    const musicRequest = new GenerateMusicRequest();
-    musicRequest.setNotesList(
-      this.state.recording.map((frame) => {
-        // must convert numerical notes to MIDI for Sarosh's library
-        const notes = frame.notes;
-        return notes.includes(".")
-          ? notes
-          : SORTED_PITCHES[+notes % 12] + Math.floor(+notes / 12);
-      })
-    );
+    appendToRecording = (item: {
+        notes: string;
+        start: number;
+        duration: number;
+    }) => {
+        const {notes, start, duration} = item;
 
-    this.setState({ musicIsGenerating: false });
-    client.generateMusic(musicRequest, null).then((res) => {
-      this.setState({
-        generatedMusic: res.getNotesList(),
-        musicIsGenerating: false,
-      });
-    });
-  };
+        const frame: Frame = {
+            id: uuidv4(),
+            notes: notes,
+            start: start,
+            duration: duration,
+        };
 
-  render() {
-    return (
-      <div className={pageStyles.container}>
-        <Head>
-          <title>Keyboard - TensorBeat</title>
-          <link rel="icon" href="/tensorbeat.svg" />
-        </Head>
+        this.setState({
+            recording: [...this.state.recording, frame],
+        });
+    };
 
-        <main className={pageStyles.main}>
-          <div className={pageStyles.section}>
-            {
-              /*
-                            Adapted from react-piano docs: https://reactjsexample.com/an-interactive-piano-keyboard-for-react/
-                            This is a poorly written codebase that is tightly coupled, I would like to rewrite it eventually
-                            */
-              this.state.audioContext && (
-                <SoundFontProvider
-                  audioContext={this.state.audioContext}
-                  hostname={soundfontHostname}
-                  render={({ isLoading, playNote, stopNote }: any) => (
-                    <PianoWithRecording
-                      appendToRecording={this.appendToRecording}
-                      noteRange={noteRange}
-                      width={PIANO_WIDTH}
-                      playNote={playNote}
-                      stopNote={stopNote}
-                      disabled={isLoading}
-                      activeNotes={this.state.activeNotes}
-                      keyboardShortcuts={keyboardShortcuts}
-                    />
-                  )}
-                />
-              )
+    clearRecording = () => {
+        this.setState({recording: []});
+    };
+
+    playRecording = () => {
+        const initialTime = this.state.recording[0].start;
+        this.state.recording.forEach(({id, notes, start, duration}) => {
+            let startDelay = start - initialTime;
+            setTimeout(() => {
+                this.setState({activeNotes: []}); // Ensure that repeated notes are played again
+                this.setState({activeNotes: notes.split(".")});
+            }, startDelay);
+            setTimeout(() => {
+                // TODO: make this work with id's. Might not be needed though
+                this.setState({
+                    activeNotes: this.state.activeNotes.filter(
+                        (an) => !notes.split(".").includes(an)
+                    ),
+                });
+            }, startDelay + duration);
+        });
+    };
+
+    playGeneratedMusic = () => {
+        this.state.generatedMusic?.forEach((note, index) => {
+            if (note.includes(".")) {
+                // chords start at 0, so transpose them up to c4
+                note = note.split(".").map(n => n + MidiNumbers.fromNote("c4")).join(".")
             }
-            <p className={boxStyles.box} style={{ width: `${PIANO_WIDTH}px` }}>
-              Current recording:
-              {this.state.recording.map((frame, index) => (
-                <span key={index}>{frame.notes} </span>
-              ))}
-            </p>
-            <div className={pageStyles.row}>
-              <button
-                className={pageStyles.button}
-                onClick={this.playRecording}
-              >
-                Play
-              </button>
-              <button
-                className={pageStyles.button}
-                onClick={this.clearRecording}
-              >
-                Clear
-              </button>
-              <button
-                className={pageStyles.button}
-                onClick={this.sendRecordingToGenerator}
-                disabled={this.state.musicIsGenerating}
-              >
-                Generate Music!
-              </button>
-              <button
-                className={pageStyles.button}
-                onClick={this.playGeneratedMusic}
-                hidden={!this.state.generatedMusic}
-              >
-                Play Generated Music
-              </button>
+            // generator outputs flats as "-"
+            note.replace("-", "b");
+            setTimeout(() => {
+                this.setState({activeNotes: []});
+                this.setState({activeNotes: note.split(".")});
+            }, index * NOTE_DURATION);
+        });
+    };
+
+    sendRecordingToGenerator = () => {
+        const client = new SaroshGeneratorClient("http://grpc-web.tensorbeat.com");
+        const musicRequest = new GenerateMusicRequest();
+        musicRequest.setNotesList(
+            this.state.recording.map((frame) => {
+                // must convert numerical notes to MIDI for Sarosh's library
+                const notes = frame.notes;
+                return notes.includes(".")
+                    ? notes
+                    : SORTED_PITCHES[+notes % 12] + Math.floor(+notes / 12);
+            })
+        );
+
+        this.setState({musicIsGenerating: false});
+        client.generateMusic(musicRequest, null).then((res) => {
+            this.setState({
+                generatedMusic: res.getNotesList(),
+                musicIsGenerating: false,
+            });
+        });
+    };
+
+    render() {
+        return (
+            <div className={pageStyles.container}>
+                <Head>
+                    <title>Keyboard - TensorBeat</title>
+                    <link rel="icon" href="/tensorbeat.svg"/>
+                </Head>
+
+                <main className={pageStyles.main}>
+                    <div className={pageStyles.section}>
+                        {
+                            /*
+                                          Adapted from react-piano docs: https://reactjsexample.com/an-interactive-piano-keyboard-for-react/
+                                          This is a poorly written codebase that is tightly coupled, I would like to rewrite it eventually
+                                          */
+                            this.state.audioContext && (
+                                <SoundFontProvider
+                                    audioContext={this.state.audioContext}
+                                    hostname={soundfontHostname}
+                                    render={({isLoading, playNote, stopNote}: any) => (
+                                        <PianoWithRecording
+                                            appendToRecording={this.appendToRecording}
+                                            noteRange={noteRange}
+                                            width={PIANO_WIDTH}
+                                            playNote={playNote}
+                                            stopNote={stopNote}
+                                            disabled={isLoading}
+                                            activeNotes={this.state.activeNotes}
+                                            keyboardShortcuts={keyboardShortcuts}
+                                        />
+                                    )}
+                                />
+                            )
+                        }
+                        <p className={boxStyles.box} style={{width: `${PIANO_WIDTH}px`}}>
+                            Current recording:
+                            {this.state.recording.map((frame, index) => (
+                                <span key={index}>{frame.notes} </span>
+                            ))}
+                        </p>
+                        <div className={pageStyles.row}>
+                            <button
+                                className={pageStyles.button}
+                                onClick={this.playRecording}
+                            >
+                                Play
+                            </button>
+                            <button
+                                className={pageStyles.button}
+                                onClick={this.clearRecording}
+                            >
+                                Clear
+                            </button>
+                            <button
+                                className={pageStyles.button}
+                                onClick={this.sendRecordingToGenerator}
+                                disabled={this.state.musicIsGenerating}
+                            >
+                                Generate Music!
+                            </button>
+                            <button
+                                className={pageStyles.button}
+                                onClick={this.playGeneratedMusic}
+                                hidden={!this.state.generatedMusic}
+                            >
+                                Play Generated Music
+                            </button>
+                        </div>
+                    </div>
+                </main>
+                <StandardFooter/>
             </div>
-          </div>
-        </main>
-        <StandardFooter />
-      </div>
-    );
-  }
+        );
+    }
 }
