ae267a02e2c8cf3d5b8787ace7b6de11d86517c7
LuLaValva <lulavalva@gmail.com>
2021-04-22 21:55:21 -0400

    Fixed clear button on piano

--- components/pianoWithRecording.js
+++ components/pianoWithRecording.js
@@ -7,74 +7,63 @@ import { Piano } from "react-piano";
 const DURATION_UNIT = 0.2;
 const DEFAULT_NOTE_DURATION = DURATION_UNIT;
 
-
-
 class PianoWithRecording extends React.Component {
-    static defaultProps = {
-        notesRecorded: false,
-    };
+  static defaultProps = {
+    notesRecorded: false,
+  };
 
-    state = {
-        noteDuration: DEFAULT_NOTE_DURATION,
-    };
+  initialTime;
+  notesBeingRecorded = {};
 
-    initialTime;
-    notesBeingRecorded = {};
+  onPlayNoteInput = (midiNumber) => {
+    this.setState({
+      notesRecorded: false,
+    });
 
-    onPlayNoteInput = (midiNumber) => {
-        this.setState({
-            notesRecorded: false,
-        });
-        const currentTime = new Date().getTime();
-        if (this.initialTime === undefined) {
-            this.initialTime = currentTime;
-        }
-        this.notesBeingRecorded[midiNumber] = currentTime - this.initialTime;
-    };
+    this.notesBeingRecorded[midiNumber] = new Date().getTime();
+  };
 
-    onStopNoteInput = (midiNumber, { prevActiveNotes }) => {
-        if (this.state.notesRecorded === false) {
-            const currentTime = new Date().getTime() - this.initialTime;
-            let key = prevActiveNotes[0] ?? prevActiveNotes
-            const start = this.notesBeingRecorded[key];
-            this.recordNotes(prevActiveNotes, start, currentTime - start);
-            this.setState({
-                notesRecorded: true,
-                noteDuration: DEFAULT_NOTE_DURATION,
-            });
-        }
-    };
-
-    recordNotes = (midiNumbers, start, duration) => {
-        this.props.appendToRecording({
-            notes: midiNumbers.join("."),
-            start: start,
-            duration: duration,
-        });
-    };
-
-    render() {
-        const {
-            playNote,
-            stopNote,
-            recording,
-            appendToRecording,
-            ...pianoProps
-        } = this.props;
-
-        return (
-            <div>
-                <Piano
-                    playNote={this.props.playNote}
-                    stopNote={this.props.stopNote}
-                    onPlayNoteInput={this.onPlayNoteInput}
-                    onStopNoteInput={this.onStopNoteInput}
-                    activeNotes={this.props.activeNotes}
-                    {...pianoProps}
-                />
-            </div>
-        );
+  onStopNoteInput = (midiNumber, { prevActiveNotes }) => {
+    if (this.state.notesRecorded === false) {
+      let key = prevActiveNotes[0] ?? prevActiveNotes;
+      const start = this.notesBeingRecorded[key];
+      this.recordNotes(prevActiveNotes, start, new Date().getTime() - start);
+      this.setState({
+        notesRecorded: true,
+      });
     }
+  };
+
+  recordNotes = (midiNumbers, start, duration) => {
+    this.props.appendToRecording({
+      notes: midiNumbers.join("."),
+      start: start,
+      duration: duration,
+    });
+  };
+
+  render() {
+    const {
+      playNote,
+      stopNote,
+      recording,
+      appendToRecording,
+      ...pianoProps
+    } = this.props;
+
+    return (
+      <div>
+        <Piano
+          playNote={this.props.playNote}
+          stopNote={this.props.stopNote}
+          onPlayNoteInput={this.onPlayNoteInput}
+          onStopNoteInput={this.onStopNoteInput}
+          activeNotes={this.props.activeNotes}
+          {...pianoProps}
+        />
+      </div>
+    );
+  }
 }
 
 export default PianoWithRecording;
--- pages/keyboard.tsx
+++ pages/keyboard.tsx
@@ -1,159 +1,164 @@
 import Head from "next/head";
-import React, {Component} from "react";
+import React, { Component } from "react";
 import pageStyles from "../styles/page.module.css";
 import boxStyles from "../styles/box.module.css";
 import StandardFooter from "../components/standardFooter";
-import {KeyboardShortcuts, MidiNumbers} from "react-piano";
+import { KeyboardShortcuts, MidiNumbers } from "react-piano";
 import "react-piano/dist/styles.css";
 import SoundFontProvider from "../components/soundFontProvider";
 import PianoWithRecording from "../components/pianoWithRecording";
 
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 const soundfontHostname = "https://d1pzp51pvbm36p.cloudfront.net";
 
 const noteRange = {
-    first: MidiNumbers.fromNote("c3"),
-    last: MidiNumbers.fromNote("f4"),
+  first: MidiNumbers.fromNote("c3"),
+  last: MidiNumbers.fromNote("f4"),
 };
 const keyboardShortcuts = KeyboardShortcuts.create({
-    firstNote: noteRange.first,
-    lastNote: noteRange.last,
-    keyboardConfig: KeyboardShortcuts.HOME_ROW,
+  firstNote: noteRange.first,
+  lastNote: noteRange.last,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW,
 });
 
 interface Frame {
-    id: string,
-    notes: string,
-    start: number,
-    duration: number,
+  id: string;
+  notes: string;
+  start: number;
+  duration: number;
 }
 
 interface KeyboardState {
-    recording: Frame[],
-    activeNotes: string[],
-    audioContext: any,
+  recording: Frame[];
+  activeNotes: string[];
+  audioContext: any;
 }
 
 const PIANO_WIDTH = 500;
 const NOTE_DURATION = 200; // milliseconds
 
 export default class Keyboard extends Component {
+  state: KeyboardState = {
+    audioContext: null,
+    recording: [],
+    activeNotes: [],
+  };
 
-    state: KeyboardState = {
-        audioContext: null,
-        recording: [],
-        activeNotes: [],
+  scheduledEvents: [];
+
+  componentDidMount = () => {
+    this.setState({
+      // audioContext: new (window.AudioContext || window.webkitAudioContext)(),
+      audioContext: new window.AudioContext(),
+    });
+  };
+
+  constructor(props) {
+    super(props);
+    this.scheduledEvents = [];
+  }
+
+  appendToRecording = (item: {
+    notes: string;
+    start: number;
+    duration: number;
+  }) => {
+    const { notes, start, duration } = item;
+
+    const frame: Frame = {
+      id: uuidv4(),
+      notes: notes,
+      start: start,
+      duration: duration,
     };
 
-    scheduledEvents: [];
+    this.setState({
+      recording: [...this.state.recording, frame],
+    });
+  };
 
-    componentDidMount = () => {
+  clearRecording = () => {
+    this.setState({ recording: [] });
+  };
+
+  playRecording = () => {
+    let initialTime = this.state.recording[0].start;
+    this.state.recording.forEach(({ id, notes, start, duration }) => {
+      let startDelay = start - initialTime;
+      setTimeout(() => {
+        this.setState({ activeNotes: [] }); // Ensure that repeated notes are played again
+        this.setState({ activeNotes: notes.split(".") });
+      }, startDelay);
+      setTimeout(() => {
+        // TODO: make this work with id's. Might not be needed though
         this.setState({
-            // audioContext: new (window.AudioContext || window.webkitAudioContext)(),
-            audioContext: new window.AudioContext()
+          activeNotes: this.state.activeNotes.filter(
+            (an) => !notes.split(".").includes(an)
+          ),
         });
-    };
+      }, startDelay + duration);
+    });
+  };
 
-    constructor(props) {
-        super(props);
-        this.scheduledEvents = [];
-    }
+  render() {
+    return (
+      <div className={pageStyles.container}>
+        <Head>
+          <title>Keyboard - TensorBeat</title>
+          <link rel="icon" href="/tensorbeat.svg" />
+        </Head>
 
-    appendToRecording = (item: {notes: string, start: number, duration: number}) => {
-
-        const {notes, start, duration} = item;
-
-        const frame: Frame = {
-            id: uuidv4(),
-            notes: notes,
-            start: start,
-            duration: duration,
-        }
-
-        this.setState({
-            recording: [...this.state.recording, frame],
-        });
-
-    };
-
-    clearRecording = () => {
-        this.setState({recording: []});
-    };
-
-    playRecording = () => {
-        this.state.recording.forEach(({id, notes, start, duration}) => {
-            setTimeout(() => {
-                this.setState({activeNotes: []}); // Ensure that repeated notes are played again
-                this.setState({activeNotes: notes.split(".")});
-            }, start);
-            setTimeout(() => {
-                // TODO: make this work with id's. Might not be needed though
-                this.setState({activeNotes: this.state.activeNotes.filter(an => !notes.split(".").includes(an))});
-            }, start + duration);
-        });
-    };
-
-    render() {
-        return (
-            <div className={pageStyles.container}>
-                <Head>
-                    <title>Keyboard - TensorBeat</title>
-                    <link rel="icon" href="/tensorbeat.svg"/>
-                </Head>
-
-                <main className={pageStyles.main}>
-                    <div className={pageStyles.section}>
-                        {
-                            /*
+        <main className={pageStyles.main}>
+          <div className={pageStyles.section}>
+            {
+              /*
                             Adapted from react-piano docs: https://reactjsexample.com/an-interactive-piano-keyboard-for-react/
                             This is a poorly written codebase that is tightly coupled, I would like to rewrite it eventually
                             */
-                            this.state.audioContext && (
-                                <SoundFontProvider
-                                    audioContext={this.state.audioContext}
-                                    hostname={soundfontHostname}
-                                    render={({isLoading, playNote, stopNote}) => (
-                                        <PianoWithRecording
-                                            appendToRecording={this.appendToRecording}
-                                            noteRange={noteRange}
-                                            width={PIANO_WIDTH}
-                                            playNote={playNote}
-                                            stopNote={stopNote}
-                                            disabled={isLoading}
-                                            activeNotes={this.state.activeNotes}
-                                            keyboardShortcuts={keyboardShortcuts}
-                                        />
-                                    )}
-                                />
-                            )
-                        }
-                        <p className={boxStyles.box} style={{width: `${PIANO_WIDTH}px`}}>
-                            Current recording:
-                            {
-                                this.state.recording.map(frame => (
-                                    <span>{frame.notes} </span>
-                                ))
-                            }
-                        </p>
-                        <div className={pageStyles.row}>
-                            <button
-                                className={pageStyles.button}
-                                onClick={this.playRecording}
-                            >
-                                Play
-                            </button>
-                            <button
-                                className={pageStyles.button}
-                                onClick={this.clearRecording}
-                            >
-                                Clear
-                            </button>
-                        </div>
-                    </div>
-                </main>
-                <StandardFooter/>
+              this.state.audioContext && (
+                <SoundFontProvider
+                  audioContext={this.state.audioContext}
+                  hostname={soundfontHostname}
+                  render={({ isLoading, playNote, stopNote }) => (
+                    <PianoWithRecording
+                      appendToRecording={this.appendToRecording}
+                      noteRange={noteRange}
+                      width={PIANO_WIDTH}
+                      playNote={playNote}
+                      stopNote={stopNote}
+                      disabled={isLoading}
+                      activeNotes={this.state.activeNotes}
+                      keyboardShortcuts={keyboardShortcuts}
+                    />
+                  )}
+                />
+              )
+            }
+            <p className={boxStyles.box} style={{ width: `${PIANO_WIDTH}px` }}>
+              Current recording:
+              {this.state.recording.map((frame) => (
+                <span>{frame.notes} </span>
+              ))}
+            </p>
+            <div className={pageStyles.row}>
+              <button
+                className={pageStyles.button}
+                onClick={this.playRecording}
+              >
+                Play
+              </button>
+              <button
+                className={pageStyles.button}
+                onClick={this.clearRecording}
+              >
+                Clear
+              </button>
             </div>
-        );
-    }
+          </div>
+        </main>
+        <StandardFooter />
+      </div>
+    );
+  }
 }
