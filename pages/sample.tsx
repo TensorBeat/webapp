8ae776d62ac3a59106aa7bdb2676f355450d1237
M Sarosh Khan <msk15163@gmail.com>
2021-05-07 02:14:28 -0500

    Added Sample Tracks

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -6,21 +6,101 @@ import { SaroshGeneratorClient } from "../grpc-web/tensorbeat/Sarosh_genServiceC
 import { GenerateMusicRequest } from "../grpc-web/tensorbeat/sarosh_gen_pb";
 
 const cloud_bucket_songs = [
-    {
+   {
+        name: "Boss Music",
+	url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Boss%20Music.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+   {
+        name: "GSU Theme",
         url:
-            "https://storage.googleapis.com/sarosh-gen/Get%20Up%20Offa%20That%20Thing-255779303.mp3",
-        name: "Get Up Offa That Thing",
+            "https://storage.googleapis.com/sarosh-gen/mp3s/GSU_Theme.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+     {
+        name: "Recomposed",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Recomposed.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+   {
+        name: "Recomposed 2",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Recomposed%202.mp3",
         meta: {
             "field 1": "information",
             "field 2": "more information",
         },
     },
     {
-        name: "Wenn ich jodle, bin ich glücklich",
+        name: "Jazz Conditioned",
         url:
-            "https://storage.googleapis.com/sarosh-gen/Wenn%20ich%20jodle%2C%20bin%20ich%20gl%C3%BCcklich%20-%20Franzl%20Lang.mp3",
+            "https://storage.googleapis.com/sarosh-gen/mp3s/jazz_v2.mp3",
         meta: {
             info: "My Favorite Franzl Lang song",
+        },
+    },
+    {
+        name: "Pop Conditioned 2",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Happier.mp3",
+        meta: {
+            info: "My Favorite Franzl Lang song",
+        },
+    },
+    {
+        name: "Ominous Overfitting",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Ominous_still_overfits.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+    {
+        name: "Luke's Piano",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Luke%20Faceroll.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+    {
+        name: "Pop Conditioned",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/Pop%20Conditioned.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+    {
+        name: "Test Output 3",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/test_output_3.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+   {
+        name: "Test Output 5",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/mp3s/test_output%20_5_condensed.mp3",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
         },
     },
 ];
