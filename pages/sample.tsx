73bf058ea95f94829e0c7e073cd2f1e37c88cd41
LuLaValva <lulavalva@gmail.com>
2021-05-06 21:29:14 -0400

    More hardcoding functionality

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -10,7 +10,18 @@ const cloud_bucket_songs = [
         url:
             "https://storage.googleapis.com/sarosh-gen/Get%20Up%20Offa%20That%20Thing-255779303.mp3",
         name: "Get Up Offa That Thing",
-        details: "A song by James Brown that I think is pretty funny",
+        meta: {
+            "field 1": "information",
+            "field 2": "more information",
+        },
+    },
+    {
+        name: "Wenn ich jodle, bin ich glücklich",
+        url:
+            "https://storage.googleapis.com/sarosh-gen/Wenn%20ich%20jodle%2C%20bin%20ich%20gl%C3%BCcklich%20-%20Franzl%20Lang.mp3",
+        meta: {
+            info: "My Favorite Franzl Lang song",
+        },
     },
 ];
 
@@ -30,18 +41,27 @@ export default function Generator() {
                         Here are some of our favorite songs that we've generated
                         so far.
                     </p>
-                </div>
 
-                {cloud_bucket_songs.map((song, index) => (
-                    <div key={index}>
-                        {song.name}:
-                        <audio controls>
-                            <source src={song.url} type="audio/mpeg" />
-                            Your browser does not support the audio element.
-                        </audio>
-                        <p>{song.details}</p>
-                    </div>
-                ))}
+                    {cloud_bucket_songs.map((song, index) => (
+                        <div key={index}>
+                            <div className={pageStyles.row}>
+                                <h3 style={{ marginRight: "1em" }}>
+                                    {song.name}
+                                </h3>
+                                <audio controls>
+                                    <source src={song.url} type="audio/mpeg" />
+                                    Your browser does not support the audio
+                                    element.
+                                </audio>
+                            </div>
+                            {Object.keys(song.meta).map((name, index) => (
+                                <p key={index}>
+                                    {name}: {song.meta[name]}
+                                </p>
+                            ))}
+                        </div>
+                    ))}
+                </div>
 
                 <div
                     className={pageStyles.section}
